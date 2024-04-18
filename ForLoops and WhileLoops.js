2ec945d5953f3e88723f7edb6745ac8725f64b3a
Anton Tsvetanov <anton.tsvetanov@scalefocus.com>
2024-04-18 11:19:16 +0300

    Loops
    Added For loop and While loop with explanations

---
+++ ForLoops and WhileLoops.js
@@ -0,0 +1,19 @@
+const names = ["Ed", "John","Maria","Dimitar","Slavi","Toni","Sasho"]
+
+for(name of names ) {
+    console.log(name);
+
+   if(name === "Maria") {
+    console.log("Maria is on my list");
+    // now after we found our record we need to stop the cycle, so we will add break; 
+   // break;
+   }
+
+   let loading = 0; 
+
+   while(loading < 5) { 
+    console.log("Website is still loading...");
+    loading ++; 
+   }
+
+}
